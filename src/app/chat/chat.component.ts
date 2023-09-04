7ba182d4872ecc153605ab5e9373feedaf436ee5
Hamza Ashfaq <hamza2.idenbrid@gmail.com>
2023-09-04 10:45:21 +0500

    chages done

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
-
+export class ChatComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    $(document).ready(function() {
+      $('.menu .item').tab();
+    });
+  }
+  
 }
